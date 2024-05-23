87c9286df1d6c85289d4d31f038c7685b57e44ac
Obinna Isiwekpeni <obinnaisiwekpeni@Obinnas-MacBook-Pro.local>
2024-05-23 22:55:06 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IronQr;
+using IronSoftware.Drawing;
+
+public class Program{
+    static void Main(string[] args){
+        var options = new QrOptions(QrErrorCorrectionLevel.High);
+
+        var myQr = QrWriter.Write("https://www.omoqo.com", options);
+        var style = new QrStyleOptions{
+            Dimensions = 300,
+            Margins = 10,
+            Color = Color.Black,
+        };
+        var qrImage = myQr.Save(style);
+        qrImage.SaveAs("omoqo.png");
+    }
+}
